7d83ff43c8f489b0167cc048417a0e01a7dde65d
Rugved Dighe <rugved1991@gmail.com>
2021-06-25 13:43:19 -0700

    Add files via upload
    Updated use case to fetch seller marketplaces and fetch orders for the first marketplace in the list

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
 import json
 import boto3
+from datetime import datetime, timedelta
+
 # Importing "requests" library to make HTTPS calls to SP API endpoints
 import requests
 # Importing requests_aws4auth library to calculate the AWS SigV4 signature and add it to SP API requests
@@ -22,12 +24,12 @@ def lambda_handler(event, context):
     
     # Fetch "client_id" and "client_secret" from your application in Seller Central by clicking on "View" in front of your application ID.
     client_id = 'your_application_client_id'
-    client_secret = 'your_application_client__secret'
+    client_secret = 'your_application_client_secret'
 
-    # In order to call an API for a seller, you will need to paste the refresh_token for that particular seller below. 
+    # In order to call an API for a seller, you will need to paste the refresh_token for that particular seller below. You can get refresh token for a seller using OAuth flow.
     # Otherwise, you can self-authorize your application by clicking on "Authorize" from the dropdown menu in front of your application ID in seller central. 
     # Once you click on "Generate Refresh Token", you would be able to receive a refresh token and paste it below.
-    refresh_token = 'paste_refresh_token_here'
+    refresh_token = 'sellers_refresh_token'
     
     # Create body for calling "api.amazon.com/auth/o2/token" endpoint with grant_type as "refresh_token"
     payload = {'grant_type':'refresh_token','client_id':client_id,'client_secret':client_secret,'refresh_token':refresh_token}
@@ -45,22 +47,44 @@ def lambda_handler(event, context):
 
     # Create AWS SigV4 signature with temporary credentials received from the above AssumeRole request.
     # AWS4Auth library calcuates the signature and creates a canonical string to be added in the "Authorization" header. It also adds x-amz-date and x-amz-security-token to the headers.
+    # If you want to call the EU or FE endpoint, change region in the parameters to "eu-west-1" or "us-west-2". For more information refer to this link: https://github.com/amzn/selling-partner-api-docs/blob/main/guides/en-US/developer-guide/SellingPartnerApiDeveloperGuide.md#selling-partner-api-endpoints
     auth = AWS4Auth(AccessKeyId, SecretAccessKey, 'us-east-1', 'execute-api',
                         session_token=SessionToken)
+    
+    # Make a call to the SP API Sellers API endpoint with headers and AWS Signature Auth
+    # If you want to call the EU or FE endpoint, change the SP API endpoint to "sellingpartnerapi-eu" or "sellingpartnerapi-fe". For more information refer to this link: https://github.com/amzn/selling-partner-api-docs/blob/main/guides/en-US/developer-guide/SellingPartnerApiDeveloperGuide.md#selling-partner-api-endpoints
+    sellersResponse = requests.get("https://sellingpartnerapi-na.amazon.com/sellers/v1/marketplaceParticipations", headers=headers, auth=auth)
+    
+    # Uncomment to print out the Sellers API request URL.
+    #print("Request URL: ", sellersResponse.request.url)
 
-    # Add parameters for the GetOrders API test call
-    params = {'CreatedAfter': '2020-01-19T03:58:30Z', 'MarketplaceIds': 'ATVPDKIKX0DER'}
+    # Uncomment to print out the Headers added to the Sellers API request URL.
+    #print("Request Headers: ", sellersResponse.request.headers)
     
-    # Make a test call to the SP API Orders API endpoint with parameters, headers and AWS Signature Auth
-    spapi = requests.get("https://sellingpartnerapi-na.amazon.com/orders/v0/orders", params=params, headers=headers, auth=auth)
+    print("Seller participates in the following marketplaces: ", sellersResponse.text)
     
-    # Uncomment to print out the SP API request URL .
-    #print("Request URL: ", spapi.request.url)
+    # Fetch first marketplace Id from the list of supported marketplaces within the authorized region.
+    marketplaceId = sellersResponse.json()['payload'][0]['marketplace']['id']
+    
+    # Get timestamp for 2 hours from now to fetch all orders within the last two hours
+    createdAfter = (datetime.utcnow() - timedelta(hours=2)).isoformat()
+    
+    #print(createdAfter)
+    
+    # Add parameters for the GetOrders API call    
+    params = {'CreatedAfter': createdAfter, 'MarketplaceIds': marketplaceId}
+    
+    # Make a call to the SP API Orders API endpoint with params, headers and AWS Signature Auth
+    # If you want to call the EU or FE endpoint, change the SP API endpoint to "sellingpartnerapi-eu" or "sellingpartnerapi-fe". For more information refer to this link: https://github.com/amzn/selling-partner-api-docs/blob/main/guides/en-US/developer-guide/SellingPartnerApiDeveloperGuide.md#selling-partner-api-endpoints
+    ordersResponse = requests.get("https://sellingpartnerapi-na.amazon.com/orders/v0/orders", params=params, headers=headers, auth=auth)
+    
+    # Uncomment to print out the Orders API request URL .
+    #print("Request URL: ", ordersResponse.request.url)
 
-    # Uncomment to print out the Headers added to the request URL.
-    #print("Request Headers: ", spapi.request.headers)
+    # Uncomment to print out the Headers added to the Orders API request URL.
+    #print("Request Headers: ", ordersResponse.request.headers)
     
-    print("Response: ", spapi.text)
+    print("Orders from the first Marketplace supported by seller: ", ordersResponse.text)
 
     return {
         'statusCode': 200,
